48be753dea2f515bd933f2fbe6871777f7c9fabb
IshDeshpa <ishdeshpa@gmail.com>
2025-01-28 23:14:54 +0000

    devcontainer setup

--- tests/main.c
+++ tests/main.c
@@ -1,5 +1,14 @@
 #include "stm32xx_hal.h"
 
+#include "pedals.h"
+
+void drivers_init(){
+    // contactors_init();
+    // display_init();
+    // motor_controller_init();
+    pedals_init();
+}
+
 int main(){
-    
+
 }
